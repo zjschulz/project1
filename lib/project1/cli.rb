71b13e76c2ca4a56d83884735f227662da5df187
Zachary Schulz <zjschulz@gmail.com>
2019-10-25 01:05:15 +0000

    more changes

--- lib/project1/cli.rb
+++ lib/project1/cli.rb
@@ -3,11 +3,21 @@ require './lib/project1'
 class Project1::CLI 
 
   def call
-    puts "Initiating Project One"
+    puts "Welcome to the D&D Class Spell Book!"
+    #menu to pick class > pick spells > view spell info
+    list_class
+    menu
   end
   
   def list_class
     #method used to list classes whom use spells
+    puts "List of Classes"
+    puts <<-DOC.gsub /^\s*/, ''
+    1. Paladin
+    2. Druid
+    3. Cleric
+    DOC
+    #bard,cleric,druid,paladin,ranger,sorcerer,warlock,wizard
   end
   
   def list_spells
@@ -20,6 +30,23 @@ class Project1::CLI
   
   def menu
     #method used to dig into lists to obtain info
+    input = nil 
+    while input != "exit"
+      puts "Enter the number of the class"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "List of Paladin Spells"
+      when "2"
+        puts "List of Druid Spells"
+      when "3"
+        puts "List of Cleric Spells"
+      when "list"
+        list_class
+      else
+        puts "Unsure of input, type list or exit"
+      end
+    end
   end
 
 end
