3bda29daf20f7427e4c2589c85576e70a47455d2
Zachary Schulz <zjschulz@gmail.com>
2019-10-31 23:50:19 +0000

    improved cli

--- lib/project1/cli.rb
+++ lib/project1/cli.rb
@@ -3,6 +3,7 @@ require './lib/project1'
 class Project1::CLI 
 
   def call
+    Project1::Horse.create_from_collection(horses = Project1::Scrape.scrape)
     puts "Welcome to the Retired Racehorse Project!"
     list_horse
     menu
@@ -11,22 +12,25 @@ class Project1::CLI
   
   def list_horse
     puts "Here are today's horse listings"
-    Project1::Horse.all
-    #returns a list of the horse listing by an index number, name, age, location, price
+    @horse = Project1::Horse.all
+    @horse.each.with_index(1) do |horse,i|
+      puts "#{i}. #{horse.name} - #{horse.birthyear} - #{horse.city}, #{horse.state} - #{horse.price}"
+    end
   end
   
   def menu
     #method used to dig into lists to obtain info
     input = nil 
     while input != "exit"
-      puts "Enter the number of the hore, type list for all listings, or type exit to leave"
+      puts "Enter the number of the horse, type list for all listings, or type exit to leave"
       input = gets.strip.downcase
       
       if input.to_i > 0 
-        thespell = @horses[input.to_i-1]
-        puts "#{thespell.name} - #{thespell.desc}"
+        x = @horse[input.to_i-1]
+        puts "#{x.name}"
+        puts "#{x.desc}"
       elsif input == "list"
-        list_job
+        list_horse
       else
         puts "Unsure of input, type list or exit"
       end
